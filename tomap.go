281b3abe5e1392a36ae9cc6f3faf9e42ef683b7e
Osama <iosamaify@gmail.com>
2020-12-23 13:18:19 +0200

    ToMap: use field name as default value for map key

--- tomap.go
+++ tomap.go
@@ -1,10 +1,11 @@
 package structs
 
 // ToMap converts a struct to a map.
-// tag is defined in struct's tags, used as map key.
+// tag is defined in struct's tags, used as map key if not empty
+// otherwise field name is used.
 // Panics if s is not a struct.
-func ToMap(s interface{}, tag string) (map[string]interface{}, error) {
-	m := map[string]interface{}{}
+func ToMap(s interface{}, tag string) map[string]interface{} {
+	m := make(map[string]interface{})
 
 	v := structValue(s)
 	t := v.Type()
@@ -12,9 +13,16 @@ func ToMap(s interface{}, tag string) (map[string]interface{}, error) {
 	for i := 0; i < v.NumField(); i++ {
 		tf := t.Field(i)
 
-		tn, to := parseTag(tf.Tag.Get(tag))
-		if tn == "" || tn == "-" {
-			continue
+		tn, to := tf.Name, tagOptions("")
+		if tag != "" {
+			n, o := parseTag(tf.Tag.Get(tag))
+			if n == "-" {
+				continue
+			}
+			if n != "" {
+				tn = n
+			}
+			to = o
 		}
 
 		vf := v.Field(i)
@@ -25,5 +33,5 @@ func ToMap(s interface{}, tag string) (map[string]interface{}, error) {
 		m[tn] = vf.Interface()
 	}
 
-	return m, nil
+	return m
 }
